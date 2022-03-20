2da6573b031e69fd9ef65c81c5f075e6ff0e174d
armanahmd <92032906+armanahmd@users.noreply.github.com>
2022-03-20 22:19:02 +0500

    Add project files.

---
+++ FibonacciSeriesSimple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FibonacciSeriesSimple
+{
+    class Program
+    {
+       static int Firstnum = 0, SecondNum = 1, Final;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter Your number");
+            int UserNum = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < UserNum; i++)
+            {
+                Final = Firstnum + SecondNum;
+                Console.WriteLine(Final);
+
+                Firstnum = SecondNum;
+                SecondNum = Final;
+
+
+            } 
+            
+
+
+
+
+        }
+    }
+}
